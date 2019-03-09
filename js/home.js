22b97639b36a7cc43bc5e9d634b9f168af6aa277
Binh Pham <thanhbinh0995@gmail.com>
2019-03-09 19:59:15 +0700

    completed

--- js/home.js
+++ js/home.js
@@ -2,8 +2,32 @@ $(document).ready(function () {
 
     $('.email-container input#email').focus(function () {
         $('.email-container input#end-link').addClass('border')
-    }).focusout(function(){
+    }).focusout(function () {
         $('.email-container input#end-link').removeClass('border')
+    });
+
+    function closeNav() {
+        $(".navbar-collapse").stop().animate({'height': '100px'}, 300, function () {
+            $('.navbar-collapse').removeClass('in').addClass("collapse");
+        });
+        $(".navbar-toggle").stop().removeClass('in').addClass('collapsed');
+
+    }
+
+    function openNav() {
+        $(".navbar-collapse").stop().animate({'height': '300px'}, 300, function () {
+            $('.navbar-collapse').removeClass("collapse").addClass('in');
+        });
+        $(".navbar-toggle").stop().removeClass('collapsed').addClass('in');
+    }
+
+    $('.navbar-collapse').click(function (event) {
+        var isOpen = $(this).hasClass("navbar-collapse in");
+        if (isOpen === true) {
+            closeNav();
+        } else {
+            openNav();
+        }
     });
 
 });
